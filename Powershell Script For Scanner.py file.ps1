1dd81a67a84addfabb5e49140f3b52ce77183503
Hardik Shah <89474886+Hardik-26@users.noreply.github.com>
2021-09-25 23:50:57 +0530

    Add files via upload

---
+++ Powershell Script For Scanner.py file.ps1
@@ -0,0 +1,18 @@
+$deviceManager = new-object -ComObject WIA.DeviceManager
+$device = $deviceManager.DeviceInfos.Item(1).Connect()    
+
+$wiaFormatPNG = "{B96B3CAF-0728-11D3-9D7B-0000F81EF32E}"
+
+foreach ($item in $device.Items) { 
+    $image = $item.Transfer($wiaFormatPNG) 
+}    
+
+if($image.FormatID -ne $wiaFormatPNG)
+{
+    $imageProcess = new-object -ComObject WIA.ImageProcess
+    $imageProcess.Filters.Add($imageProcess.FilterInfos.Item("Convert").FilterID)
+    $imageProcess.Filters.Item(1).Properties.Item("FormatID").Value = $wiaFormatPNG
+    $image = $imageProcess.Apply($image)
+}
+
+$image.SaveFile("C:\Users\denni\Desktop\auauau.png")
---
+++ scanner.py
@@ -0,0 +1,86 @@
+#|==============================================================================================
+#|                                                                                         Scanner Module                                                                                                         |
+#|==============================================================================================
+
+# Module By : Hardik Shah.
+
+#|---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# With this module you will be able to use your flatbed scanner using python.
+# This module uses PowerShell script to execute the scan command.
+# you can say that this module works as an API ith Powershell.
+#
+# INSTRUCTIONS-
+# Make sure your flatbed scanner is pluged in and ready to scan
+# Change the Execution Policy in Powershell for your computer.
+# well thats it.
+#
+# HOW TO USE-
+# import scanner
+# scanner.StartScan("Path"( where you want your image file to be saved) , "ImageName" )
+# Ex-  scanner.StartScan('C:\Users\\Admin\\Desktop','TestIamge')
+#
+# THANK YOU
+#|---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+# Scanning-----------------------------------------------------------------------------------------
+
+def StartScan(Path,ImageName):
+
+    # IMPORTS------------------
+    import subprocess
+    import os
+    #------------------------------
+
+    if os.path.exists(Path)==False:
+        raise OSError(" Path Not Found / Path Does Not Exist ")
+    else:
+        pass
+
+    #POWERSHELL SCRIPT ---------------------------------------------------------------------------
+    Powershell_code='''$deviceManager = new-object -ComObject WIA.DeviceManager
+$device = $deviceManager.DeviceInfos.Item(1).Connect()    
+
+$wiaFormatPNG = "{B96B3CAF-0728-11D3-9D7B-0000F81EF32E}"
+
+foreach ($item in $device.Items) { 
+    $image = $item.Transfer($wiaFormatPNG) 
+}    
+
+if($image.FormatID -ne $wiaFormatPNG)
+{
+    $imageProcess = new-object -ComObject WIA.ImageProcess
+    $imageProcess.Filters.Add($imageProcess.FilterInfos.Item("Convert").FilterID)
+    $imageProcess.Filters.Item(1).Properties.Item("FormatID").Value = $wiaFormatPNG
+    $image = $imageProcess.Apply($image)
+}
+
+$image.SaveFile("'''+ str(Path)+'\\'+str(ImageName)+'.png'+'"'+')'
+
+    Powershell_code= str(Powershell_code)
+    
+#-------------------------------------------------------------------------------------------------------
+    
+    powershell_file= open('scanner.ps1','w')
+    powershell_file.write(Powershell_code)
+    powershell_file.close()
+    process = subprocess.Popen('powershell.exe -ExecutionPolicy RemoteSigned -file "scanner.ps1"', stdout= subprocess.PIPE) # Execute Powershell file
+    process.communicate()
+    #os.remove('scanner.ps1')
+
+
+#|==============================================================================================
+
+
+        
+
+
+
+
+
+
+
+
+        
