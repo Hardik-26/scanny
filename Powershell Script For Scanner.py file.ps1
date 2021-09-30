f00f3a1cfdd9f07e5dffa84b9f56b9a47258eec1
Hardik Shah <89474886+Hardik-26@users.noreply.github.com>
2021-09-30 23:54:18 +0530

    Update Powershell Script For Scanner.py file.ps1

--- Powershell Script For Scanner.py file.ps1
+++ Powershell Script For Scanner.py file.ps1
@@ -1,3 +1,23 @@
+#-------------------------------------------------------------------
+Add-Type -Name Window -Namespace Console -MemberDefinition '
+
+[DllImport("Kernel32.dll")]
+public static extern IntPtr GetConsoleWindow();
+
+[DllImport("user32.dll")]
+public static extern bool ShowWindow(IntPtr hWnd, Int32 nCmdShow);
+'
+
+function Hide-Console
+{
+    $consolePtr = [Console.Window]::GetConsoleWindow()
+    #0 hide
+    [Console.Window]::ShowWindow($consolePtr, 0)
+}
+Hide-Console
+
+#-------------------------------------------------------------------
+
 $deviceManager = new-object -ComObject WIA.DeviceManager
 $device = $deviceManager.DeviceInfos.Item(1).Connect()    
 
